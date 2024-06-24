49de86a990c49e05e52b300b68b258154e53c2da
Rakesh <rakeshhegde305@gmail.com>
2024-06-24 20:04:58 +0530

    added digit_sum

---
+++ digit_sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,rem,sum=0;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+    while(n!=0)
+        {
+            rem=n%10;
+            sum+=rem
+            n=n/10;
+        }
+    printf("THE SUM OF THE DIGITS IS =%d",sum);
+}
