2638d512879ea6227730b655f22a03bc6fc1d2da
Rakesh_Hegde <152996081+Rakesh032004@users.noreply.github.com>
2024-10-31 17:54:33 +0530

    Added solution for finding count of lower case charecters in a given string.

---
+++ Count_LowerCase.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Count_LowerCase {
+    public static void main(String[]args)
+    {
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        for (int i = 0; i < s.length(); i++) 
+        {
+            if(Character.isLowerCase(s.charAt(i)))
+            {
+                count++;
+            }
+        }
+      
+        System.out.println(count);
+    }
+}
