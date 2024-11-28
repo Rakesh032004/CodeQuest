6dd174cdef44bd27cce9c490477ef526b2f25ddd
Rakesh_Hegde <152996081+Rakesh032004@users.noreply.github.com>
2024-11-28 22:23:06 +0530

    Added Java solution for finding sum of digits in the given string.

---
+++ DigitSum.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class DigitSum{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        int sum = 0;
+        for (char c : input.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sum += c - '0';
+            }
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
