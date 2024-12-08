2a83dfe03157e6a6a1529103d3e593549c8df8c4
Rakesh_Hegde <152996081+Rakesh032004@users.noreply.github.com>
2024-12-08 19:42:49 +0530

    Added Java code for finding number of capital letters in a given string

---
+++ CapitalLetterCounter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CapitalLetterCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        int capitalCount = 0;
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                capitalCount++;
+            }
+        }
+        System.out.println("Number of capital letters: " + capitalCount);
+    }
+}
