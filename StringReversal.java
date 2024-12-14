565cd2b2718c1a19fb2d139281edd7bdf90a1017
Rakesh_Hegde <152996081+Rakesh032004@users.noreply.github.com>
2024-12-14 13:36:09 +0530

    Java program to reverse a string without built in methods.

---
+++ StringReversal.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class StringReversal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reverseString(str));
+    }
+
+    public static String reverseString(String str) {
+        char[] arr = str.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+}
